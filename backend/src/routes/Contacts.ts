f3c9aebbcc141000789a0760dc7df7fdcbbf5c8d
Pedro <xtron44@gmail.com>
2025-10-08 15:44:57 -0300

    Arquivo de rota Contacts.ts criado com exportação default de router do tipo Router() from express

---
+++ backend/src/routes/Contacts.ts
@@ -0,0 +1,107 @@
+import { Router } from "express";
+import { prisma } from "../prisma";
+
+const router = Router();
+
+router.post("/", async (req, res) => {
+  const { nome, email, phone } = req.body;
+  const contacts = await prisma.contacts.findMany();
+  const emailDuplicado = contacts.filter(
+    (value: {
+      id: any;
+      nome: string;
+      email: string;
+      phone: string;
+      createdAt: Date;
+      updatedAt: Date;
+    }) => {
+      if (value.email == email) {
+        return true;
+      }
+    }
+  );
+  if (emailDuplicado) {
+    return res.status(409).json({ error: "Email duplicado!" });
+  }
+  const contact = await prisma.contacts.create({
+    data: {
+      nome: nome,
+      email: email,
+      phone: phone,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  });
+  res.json({ name: nome, email: email, phone: phone });
+});
+
+router.get("/", async (req, res) => {
+  const { q, page = 1, pageSize = 10 } = req.query;
+
+  const total = await prisma.contacts.count({
+    where: q
+      ? {
+          OR: [
+            { nome: { contains: String(q), mode: "insensitive" } },
+            { email: { contains: String(q), mode: "insensitive" } },
+          ],
+        }
+      : {},
+  });
+
+  const contacts = await prisma.contacts.findMany({
+    where: q
+      ? {
+          OR: [
+            { nome: { contains: String(q), mode: "insensitive" } },
+            { email: { contains: String(q), mode: "insensitive" } },
+          ],
+        }
+      : {},
+    skip: (Number(page) - 1) * Number(pageSize),
+    take: Number(pageSize),
+  });
+
+  res.json({
+    data: contacts,
+    page: Number(page),
+    pageSize: Number(pageSize),
+    total: total,
+  });
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { nome, email, phone } = req.body;
+  if (!nome || !email || !phone) {
+    return res.status(400).json({ error: "ValidationError" });
+  }
+  try {
+    const contacts = await prisma.contacts.findMany();
+    const contact = await prisma.contacts.update({
+      where: { id: Number(id) },
+      data: {
+        nome: nome,
+        email: email,
+        phone: phone,
+        updatedAt: new Date(),
+      },
+    });
+    res.json(contact);
+  } catch (error) {
+    res.status(500).json({ error: "InternalError" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await prisma.contacts.delete({ where: { id: Number(id) } });
+    res.json(`Contato de id ${id} deletado!`);
+  } catch (error: any) {
+    if (error.code === "P2025")
+      return res.status(400).json({ error: "Not Found" });
+    res.status(500).json({ error: "InternalError" });
+  }
+});
+export default router;
