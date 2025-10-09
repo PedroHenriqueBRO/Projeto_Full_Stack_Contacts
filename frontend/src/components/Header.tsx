2cd739377a3fb181c5809f5e3158465c3d90f36a
Pedro <xtron44@gmail.com>
2025-10-08 22:46:34 -0300

    Criação de header para a tela principal

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div className="bg-white h-[60px] w-[700px] flex items-center rounded-full justify-center gap-4">
+      <button className="font-bold text-center text-[14px]">Home</button>
+      <button className="font-bold text-center text-[14px]">About</button>
+    </div>
+  );
+}
+export default Header;
