da3bea2c634b1b718e2c099e89a8640750f41e2a
Pedro <xtron44@gmail.com>
2025-10-10 21:11:38 -0300

    feat: criação de arquivo BarraDeNavegacao e retorno de componente funcionando

---
+++ frontend/src/components/BarraDeNavegacao.tsx
@@ -0,0 +1,6 @@
+function BarraDeNavegacao(props){
+    return(<div className="col-span-2 md:col-span-1 bg-black p-4 gap-10 flex flex-col border-r-white border">
+        <button className=" hover:text-gray-500 text-white" onClick={()=>props.layout===0?props.setLayout(1):props.setLayout(0)}>Contatos</button>
+    </div>)
+}
+export default BarraDeNavegacao;
