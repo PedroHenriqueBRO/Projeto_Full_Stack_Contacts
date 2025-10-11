d98783124c017c210c3ea5d2bedfe7e707bc87b6
Pedro <xtron44@gmail.com>
2025-10-10 21:03:11 -0300

    feat: mudança total no front end, App recebe agora somente GerenciaOperacoes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,9 @@
 import Header from "./components/Header";
-import Caixadeform from "./components/Caixadeform";
-import { Smartphone } from "lucide-react";
+import BarraDeNavegacao from "./components/BarraDeNavegacao.tsx";
+import GerenciaOperacoes from "./components/GerenciaOperacoes.tsx";
 function App() {
   return (
-    <div className="bg-black w-screen h-screen flex justify-center">
-      <div className="absolute mr-410 mt-2 flex rounded-md">
-        <Smartphone size={25} className="text-indigo-600 mt-3" />
-        <h1 className="text-white text-[30px]">My Contacts</h1>
-      </div>
-      <div className="absolute">
-        <Caixadeform></Caixadeform>
-      </div>
-      <div className="absolute">
-        <Header></Header>
-      </div>
-    </div>
+      <GerenciaOperacoes></GerenciaOperacoes>
   );
 }
 
