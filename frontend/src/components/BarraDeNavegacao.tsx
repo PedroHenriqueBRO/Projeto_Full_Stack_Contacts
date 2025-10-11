dff74b86e303e9f9417a40315585eeee2799b03b
Pedro <xtron44@gmail.com>
2025-10-11 13:23:51 -0300

    feat: mudança de layout sendo feita no componente BarraDeNavegacao

--- frontend/src/components/BarraDeNavegacao.tsx
+++ frontend/src/components/BarraDeNavegacao.tsx
@@ -1,9 +1,10 @@
 function BarraDeNavegacao(props){
-    return(<div className="col-span-2 md:col-span-1 bg-black p-4 gap-10 flex flex-col border-r-white border">
+    return(<div className="col-span-3 md:col-span-1 bg-black p-4 gap-10 flex flex-col border-r-white border flex-grow">
         <button className=" hover:text-gray-500 text-white" onClick={
             async ()=> {
                 // eslint-disable-next-line @typescript-eslint/no-unused-expressions
                 if(props.layout===0) {
+                    props.setLayout(1)
                    props.getContacts("",1,10)
                 }else {
                     props.setLayout(0);
