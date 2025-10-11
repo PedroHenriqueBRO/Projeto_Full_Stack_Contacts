8071b039e7d24564bfe724bdc7bb85260247f266
Pedro <xtron44@gmail.com>
2025-10-11 00:35:32 -0300

    feat: adição de logica de pesquisa de contatos para mostrar na tela ao apertar no botão Contatos

--- frontend/src/components/BarraDeNavegacao.tsx
+++ frontend/src/components/BarraDeNavegacao.tsx
@@ -1,6 +1,19 @@
-function BarraDeNavegacao(props: { layout: number; setLayout: (arg0: number) => void; }){
+import * as React from "react";
+
+function BarraDeNavegacao(props){
     return(<div className="col-span-2 md:col-span-1 bg-black p-4 gap-10 flex flex-col border-r-white border">
-        <button className=" hover:text-gray-500 text-white" onClick={()=>props.layout===0?props.setLayout(1):props.setLayout(0)}>Contatos</button>
+        <button className=" hover:text-gray-500 text-white" onClick={
+            async ()=> {
+                // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+                if(props.layout===0) {
+                   await props.getContacts("",1,10)
+                    props.setLayout(1);
+                }else {
+                    props.setLayout(0);
+                }
+
+            }
+        }>Contatos</button>
     </div>)
 }
 export default BarraDeNavegacao;
