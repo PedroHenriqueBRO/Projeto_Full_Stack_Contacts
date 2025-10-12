5d77b5c7d4821bb493d3ad9519680ebed93edf9f
Pedro <xtron44@gmail.com>
2025-10-12 01:43:50 -0300

    style: mudança de borda e retirando o overflow x e y da tela

--- frontend/src/components/GerenciaOperacoes.tsx
+++ frontend/src/components/GerenciaOperacoes.tsx
@@ -102,9 +102,9 @@ function GerenciaOperacoes() {
   }, []);
     return(<div className="h-screen w-screen flex flex-col">
         <Header></Header>
-        <div className="grid grid-cols-8 flex-grow w-full">
+        <div className="grid grid-cols-8 flex-grow w-full overflow-x-hidden overflow-y-hidden ">
             <BarraDeNavegacao setLayout={setLayout} layout={layout} getContacts={fetchContacts}></BarraDeNavegacao>
-            <div className=" col-span-5 md:col-span-7 bg-white flex-grow w-full">
+            <div className=" col-span-5 md:col-span-7 bg-white flex-grow w-full border border-t-black">
                 <TelaDeCrud putContact={putContacts} setLayout={setLayout} layout={layout} contacts={contatos} loading={loading} delete={deleteContacts} getContacts={fetchContacts} postContact={postContacts} ></TelaDeCrud>
             </div>
         </div>
