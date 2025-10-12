f2102192e4528a0ba6915f5fa17671252d3c4355
Pedro <xtron44@gmail.com>
2025-10-12 01:42:53 -0300

    style: mudança na cor da div para bg-indigo-700

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,9 @@
 import { Smartphone } from "lucide-react";
 function Header() {
   return (
-<div className="bg-black p-6 border-b-gray-400 border ">
+<div className="bg-indigo-700 p-6 shadow-lg border ">
     <div className="flex">
-        <Smartphone className="text-purple-500"></Smartphone>
+        <Smartphone className="text-white"></Smartphone>
         <h1 className="text-white font-bold">My Contacts</h1>
     </div>
 </div>
