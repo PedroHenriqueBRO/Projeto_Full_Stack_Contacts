686a22b0eb709c834e39036f9050d50e90eb96f2
Pedro <xtron44@gmail.com>
2025-10-13 13:11:47 -0300

    Adição de arquivo ContactsInterfaces.tsx para conter as interfaces de Contact e PostContact

---
+++ frontend/src/components/ContactsInterfaces.tsx
@@ -0,0 +1,12 @@
+export interface Contact {
+  id: number;
+  nome: string;
+  email: string;
+  phone: string;
+  createdAt: string;
+}
+export interface PostContact {
+  nome: string;
+  email: string;
+  phone: string;
+}
