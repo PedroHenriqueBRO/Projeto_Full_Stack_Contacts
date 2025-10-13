6579cd06940f711eac88c7023f8e3d4eb76c7c4e
Pedro <xtron44@gmail.com>
2025-10-13 02:23:03 -0300

    fix: mudança na organização dos contatos da seed para não estarem em ordem nem crescente e nem descrescente

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,56 +1,60 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-const seedContacts: { nome: string; email: string; phone: string }[] = [
-  {
-    nome: "Alice Silva",
-    email: "alice.silva@exemplo.com",
-    phone: "11987654321",
-  },
-  {
-    nome: "Bruno Costa",
-    email: "bruno.costa@exemplo.com",
-    phone: "21912345678",
-  },
-  {
-    nome: "Carla Souza",
-    email: "carla.souza@exemplo.com",
-    phone: "31901010101",
-  },
-  {
-    nome: "Daniel Pereira",
-    email: "daniel.pereira@exemplo.com",
-    phone: "41920202020",
-  },
-  { nome: "Elisa Lima", email: "elisa.lima@exemplo.com", phone: "51930303030" },
-  {
-    nome: "Felipe Rocha",
-    email: "felipe.rocha@exemplo.com",
-    phone: "61940404040",
-  },
-  {
-    nome: "Giovanna Alves",
-    email: "giovanna.alves@exemplo.com",
-    phone: "71950505050",
-  },
-  {
-    nome: "Henrique Gomes",
-    email: "henrique.gomes@exemplo.com",
-    phone: "81960606060",
-  },
-  {
-    nome: "Isabela Santos",
-    email: "isabela.santos@exemplo.com",
-    phone: "91970707070",
-  },
-  {
-    nome: "Júlio Ferreira",
-    email: "julio.ferreira@exemplo.com",
-    phone: "11980808080",
-  },
+const seedContactsEmbaralhados:{nome:string,email:string,phone:string}[] = [
+    {
+        nome: "Daniel Pereira",
+        email: "daniel.pereira@exemplo.com",
+        phone: "+55 41 92020 2020",
+    },
+    {
+        nome: "Júlio Ferreira",
+        email: "julio.ferreira@exemplo.com",
+        phone: "+55 11 98080 8080",
+    },
+    {
+        nome: "Carla Souza",
+        email: "carla.souza@exemplo.com",
+        phone: "+55 31 90101 0101",
+    },
+    {
+        nome: "Henrique Gomes",
+        email: "henrique.gomes@exemplo.com",
+        phone: "+55 81 96060 6060",
+    },
+    {
+        nome: "Alice Silva",
+        email: "alice.silva@exemplo.com",
+        phone: "+55 11 98765 4321",
+    },
+    {
+        nome: "Giovanna Alves",
+        email: "giovanna.alves@exemplo.com",
+        phone: "+55 71 95050 5050",
+    },
+    {
+        nome: "Felipe Rocha",
+        email: "felipe.rocha@exemplo.com",
+        phone: "+55 61 94040 4040",
+    },
+    {
+        nome: "Elisa Lima",
+        email: "elisa.lima@exemplo.com",
+        phone: "+55 51 93030 3030"
+    },
+    {
+        nome: "Bruno Costa",
+        email: "bruno.costa@exemplo.com",
+        phone: "+55 21 91234 5678",
+    },
+    {
+        nome: "Isabela Santos",
+        email: "isabela.santos@exemplo.com",
+        phone: "+55 91 97070 7070",
+    },
 ];
 async function main() {
-  for (const contato of seedContacts) {
+  for (const contato of seedContactsEmbaralhados) {
     await prisma.contact.upsert({
       where: { email: contato.email },
       update: {},
