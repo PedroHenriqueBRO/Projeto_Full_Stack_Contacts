783018c1fa1e8db6d30a35f368c360032c5263d8
Pedro <xtron44@gmail.com>
2025-10-13 03:32:54 -0300

    style: mudança de cor do bg do header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { Smartphone } from "lucide-react";
 function Header() {
   return (
-<div className="bg-indigo-700 p-6 shadow-lg border ">
+<div className="bg-[#49708a] p-6 shadow-lg ">
     <div className="flex">
         <Smartphone className="text-white"></Smartphone>
         <h1 className="text-white font-bold">My Contacts</h1>
