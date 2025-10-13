f33f224d6699bfcf946d7f1c7c029fbb906caca9
Pedro <xtron44@gmail.com>
2025-10-13 00:33:32 -0300

    style: alteração no style da barra de navegação alterando cor , posicionamento de labels e style desses mesmos labels

--- frontend/src/components/BarraDeNavegacao.tsx
+++ frontend/src/components/BarraDeNavegacao.tsx
@@ -1,6 +1,10 @@
+import {User} from "lucide-react"
 function BarraDeNavegacao(props){
-    return(<div className="col-span-3 md:col-span-1 bg-gray-800 p-4 gap-10 flex flex-col border flex-grow">
-        <button className=" hover:text-gray-500 text-gray-200" onClick={
+    return(<div className="col-span-3 md:col-span-1 bg-[#49708a] gap-10 flex flex-col w-full h-full flex-grow border border-r-0 border-l-0 border-b-0 border-t-black">
+        <div className={" flex  flex-col w-full gap-1"}>
+            <div className={"ml-2 flex flex-row justify-center"} >
+                <User className={"mt-1 text-white"}></User>
+                <button className=" hover:text-gray-500 text-white " onClick={
             async ()=> {
                 // eslint-disable-next-line @typescript-eslint/no-unused-expressions
                 if(props.layout===0) {
@@ -11,7 +15,7 @@ function BarraDeNavegacao(props){
                 }
 
             }
-        }>Contatos</button>
+        }><h1 className={"text-[20px]"}>Contatos</h1></button></div></div>
     </div>)
 }
 export default BarraDeNavegacao;
