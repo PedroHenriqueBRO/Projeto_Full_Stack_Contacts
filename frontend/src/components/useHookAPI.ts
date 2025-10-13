9822776f2fb59e4d4abd8375277bd0019de4dc9b
Pedro <xtron44@gmail.com>
2025-10-13 13:59:36 -0300

    feat: adição de arquivo useHookAPI que contém os estados principais da tela para serem usados em GerenciaOperacoes.tsx e também as funções de CRUD

---
+++ frontend/src/components/useHookAPI.ts
@@ -0,0 +1,107 @@
+import { useCallback, useState } from "react";
+import type { Contact, PostContact } from "./ContactsInterfaces.tsx";
+export default function useHookAPI() {
+  const [contatos, setContatos] = useState<Contact[]>([]);
+  const [layout, setLayout] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false);
+  const fetchContacts = useCallback(
+    async (
+      name: string,
+      page: number | undefined,
+      pagesize: number | undefined,
+      sort: string,
+      order: string
+    ) => {
+      const url = `http://localhost:8082/contacts?q=${name}&page=${page}&pageSize=${pagesize}&sort=${sort}&order=${order}`;
+      setLoading(true);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Falha ao buscar contatos na API.");
+        }
+        const data = await response.json();
+        setContatos(data.data as Contact[]);
+      } catch (error) {
+        console.error("Erro na busca:", error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [setLoading, setContatos]
+  );
+  const postContacts = useCallback(
+    async (contato: PostContact) => {
+      const url = `http://localhost:8082/contacts`;
+      setLoading(true);
+      try {
+        const response = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(contato),
+        });
+        if (!response.ok) {
+          throw new Error("Falha ao tentar criar contato na API.");
+        }
+      } catch (error) {
+        console.error("Erro na criação:", error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [setLoading]
+  );
+  const putContacts = useCallback(
+    async (contato: Contact, id: number) => {
+      const url = `http://localhost:8082/contacts/${id}`;
+      setLoading(true);
+      try {
+        const response = await fetch(url, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(contato),
+        });
+        if (!response.ok) {
+          throw new Error("Falha ao tentar atualizar contato na API.");
+        }
+      } catch (error) {
+        console.error("Erro na atualização:", error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [setLoading]
+  );
+  const deleteContacts = useCallback(
+    async (id: number) => {
+      const url = `http://localhost:8082/contacts/${id}`;
+      setLoading(true);
+      try {
+        const response = await fetch(url, {
+          method: "DELETE",
+        });
+        if (!response.ok) {
+          throw new Error("Falha ao tentar deletar contato na API.");
+        }
+      } catch (error) {
+        console.error("Erro na deleção:", error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [setLoading]
+  );
+  return {
+    fetchContacts,
+    postContacts,
+    putContacts,
+    deleteContacts,
+    contatos,
+    layout,
+    setLayout,
+    loading,
+  };
+}
