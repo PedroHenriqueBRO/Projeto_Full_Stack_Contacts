58036c0b10ac473768a78888d592a03b2f735cea
Pedro <xtron44@gmail.com>
2025-10-14 11:24:15 -0300

    refactor: melhor identação

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,12 @@
 import { Smartphone } from "lucide-react";
 function Header() {
-  return (
-<div className="bg-[#49708a] p-6 shadow-lg ">
-    <div className="flex">
-        <Smartphone className="text-white"></Smartphone>
-        <h1 className="text-white font-bold">My Contacts</h1>
-    </div>
-</div>
-  );
+    return (
+        <div className="bg-[#49708a] p-6 shadow-lg ">
+            <div className="flex">
+                <Smartphone className="text-white"></Smartphone>
+                <h1 className="text-white font-bold">My Contacts</h1>
+            </div>
+        </div>
+    );
 }
 export default Header;
